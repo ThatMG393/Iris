297b32043b65a23e47012968cd8ebcd94dabc34d
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-01-13 12:00:50 +0800

    Update ModelToEntityVertexSerializer.java

--- common/src/main/java/net/irisshaders/iris/vertices/sodium/ModelToEntityVertexSerializer.java
+++ common/src/main/java/net/irisshaders/iris/vertices/sodium/ModelToEntityVertexSerializer.java
@@ -13,54 +13,53 @@ public class ModelToEntityVertexSerializer implements VertexSerializer {
     @Override
     public void serialize(long src, long dst, int vertexCount) {
         int quadCount = vertexCount / 4;
+        int entityStride = EntityVertex.STRIDE;
+        int irisStride = IrisVertexFormats.ENTITY.getVertexSize();
 
         for (int i = 0; i < quadCount; i++) {
-            int srcOffset = 0;
-            int dstOffset = 0;
-            
-            int normal = getInt(src, srcOffset + 32);
+            int normal = getInt(src, 32);
             int tangent = NormalHelper.computeTangent(
                 null,
                 NormI8.unpackX(normal),
                 NormI8.unpackY(normal),
                 NormI8.unpackZ(normal),
-                intBitsToFloat(getInt(src, srcOffset)),
-                intBitsToFloat(getInt(src, srcOffset + 4)),
-                intBitsToFloat(getInt(src, srcOffset + 8)),
-                intBitsToFloat(getInt(src, srcOffset + 16)),
-                intBitsToFloat(getInt(src, srcOffset + 20)),
-                intBitsToFloat(getInt(src, srcOffset + EntityVertex.STRIDE)),
-                intBitsToFloat(getInt(src, srcOffset + 4 + EntityVertex.STRIDE)),
-                intBitsToFloat(getInt(src, srcOffset + 8 + EntityVertex.STRIDE)),
-                intBitsToFloat(getInt(src, srcOffset + 16 + EntityVertex.STRIDE)),
-                intBitsToFloat(getInt(src, srcOffset + 20 + EntityVertex.STRIDE)),
-                intBitsToFloat(getInt(src, srcOffset + 2 * EntityVertex.STRIDE)),
-                intBitsToFloat(getInt(src, srcOffset + 4 + 2 * EntityVertex.STRIDE)),
-                intBitsToFloat(getInt(src, srcOffset + 8 + 2 * EntityVertex.STRIDE)),
-                intBitsToFloat(getInt(src, srcOffset + 16 + 2 * EntityVertex.STRIDE)),
-                intBitsToFloat(getInt(src, srcOffset + 20 + 2 * EntityVertex.STRIDE))
+                intBitsToFloat(getInt(src, 0)),
+                intBitsToFloat(getInt(src, 4)),
+                intBitsToFloat(getInt(src, 8)),
+                intBitsToFloat(getInt(src, 16)),
+                intBitsToFloat(getInt(src, 20)),
+                intBitsToFloat(getInt(src, entityStride)),
+                intBitsToFloat(getInt(src, entityStride + 4)),
+                intBitsToFloat(getInt(src, entityStride + 8)),
+                intBitsToFloat(getInt(src, entityStride + 16)),
+                intBitsToFloat(getInt(src, entityStride + 20)),
+                intBitsToFloat(getInt(src, 2 * entityStride)),
+                intBitsToFloat(getInt(src, 2 * entityStride + 4)),
+                intBitsToFloat(getInt(src, 2 * entityStride + 8)),
+                intBitsToFloat(getInt(src, 2 * entityStride + 16)),
+                intBitsToFloat(getInt(src, 2 * entityStride + 20))
             );
 
             float midU = 0, midV = 0;
             for (int vertex = 0; vertex < 4; vertex++) {
-                midU += intBitsToFloat(getInt(src, srcOffset + 16 + (EntityVertex.STRIDE * vertex)));
-                midV += intBitsToFloat(getInt(src, srcOffset + 20 + (EntityVertex.STRIDE * vertex)));
+                midU += intBitsToFloat(getInt(src, vertex * entityStride + 16));
+                midV += intBitsToFloat(getInt(src, vertex * entityStride + 20));
             }
 
             midU /= 4;
             midV /= 4;
 
             for (int j = 0; j < 4; j++) {
-                MemoryIntrinsics.copyMemory(src + srcOffset, dst + dstOffset, 36);
-                putShort(dst, dstOffset + 36, (short) CapturedRenderingState.INSTANCE.getCurrentRenderedEntity());
-                putShort(dst, dstOffset + 38, (short) CapturedRenderingState.INSTANCE.getCurrentRenderedBlockEntity());
-                putShort(dst, dstOffset + 40, (short) CapturedRenderingState.INSTANCE.getCurrentRenderedItem());
-                putInt(dst, dstOffset + 42, Float.floatToRawIntBits(midU));
-                putInt(dst, dstOffset + 46, Float.floatToRawIntBits(midV));
-                putInt(dst, dstOffset + 50, tangent);
+                MemoryIntrinsics.copyMemory(src, dst, 36);
+                putShort(dst, 36, (short) CapturedRenderingState.INSTANCE.getCurrentRenderedEntity());
+                putShort(dst, 38, (short) CapturedRenderingState.INSTANCE.getCurrentRenderedBlockEntity());
+                putShort(dst, 40, (short) CapturedRenderingState.INSTANCE.getCurrentRenderedItem());
+                putInt(dst, 42, Float.floatToRawIntBits(midU));
+                putInt(dst, 46, Float.floatToRawIntBits(midV));
+                putInt(dst, 50, tangent);
 
-                srcOffset += EntityVertex.STRIDE;
-                dstOffset += IrisVertexFormats.ENTITY.getVertexSize();
+                src += entityStride;
+                dst += irisStride;
             }
         }
     }
