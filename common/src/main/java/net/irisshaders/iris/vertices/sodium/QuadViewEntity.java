8f05ffa806567d3a2123df212492d21e925a222e
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-01-13 10:13:11 +0800

    Update QuadViewEntity.java

--- common/src/main/java/net/irisshaders/iris/vertices/sodium/QuadViewEntity.java
+++ common/src/main/java/net/irisshaders/iris/vertices/sodium/QuadViewEntity.java
@@ -2,38 +2,45 @@ package net.irisshaders.iris.vertices.sodium;
 
 import net.irisshaders.iris.vertices.views.QuadView;
 import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.system.Pointer;
 
 public class QuadViewEntity implements QuadView {
-	private long writePointer;
-	private int stride;
+    private long writePointer;
+    private int stride;
 
-	public void setup(long writePointer, int stride) {
-		this.writePointer = writePointer;
-		this.stride = stride;
-	}
+    public void setup(long writePointer, int stride) {
+        // Ensure pointer is aligned according to platform requirements
+        this.writePointer = writePointer & ~((1L << Pointer.POINTER_SHIFT) - 1);
+        this.stride = (stride + (1 << Pointer.POINTER_SHIFT) - 1) & ~((1 << Pointer.POINTER_SHIFT) - 1);
+    }
 
-	@Override
-	public float x(int index) {
-		return MemoryUtil.memGetFloat(writePointer - stride * (3L - index));
-	}
+    @Override
+    public float x(int index) {
+        return MemoryUtil.memGetFloat(getOffset(index));
+    }
 
-	@Override
-	public float y(int index) {
-		return MemoryUtil.memGetFloat(writePointer + 4 - stride * (3L - index));
-	}
+    @Override
+    public float y(int index) {
+        return MemoryUtil.memGetFloat(getOffset(index) + 4);
+    }
 
-	@Override
-	public float z(int index) {
-		return MemoryUtil.memGetFloat(writePointer + 8 - stride * (3L - index));
-	}
+    @Override
+    public float z(int index) {
+        return MemoryUtil.memGetFloat(getOffset(index) + 8);
+    }
 
-	@Override
-	public float u(int index) {
-		return MemoryUtil.memGetFloat(writePointer + 16 - stride * (3L - index));
-	}
+    @Override
+    public float u(int index) {
+        return MemoryUtil.memGetFloat(getOffset(index) + 16);
+    }
 
-	@Override
-	public float v(int index) {
-		return MemoryUtil.memGetFloat(writePointer + 20 - stride * (3L - index));
-	}
+    @Override
+    public float v(int index) {
+        return MemoryUtil.memGetFloat(getOffset(index) + 20);
+    }
+
+    private long getOffset(int index) {
+        // Ensure index arithmetic is platform-aware
+        return writePointer + ((long) stride * index);
+    }
 }
